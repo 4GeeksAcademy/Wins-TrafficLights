67ce3cf51474556eaa56d3d4078587ab85a49996
WinstonLesuT <138252771+WinstonLesuT@users.noreply.github.com>
2023-11-07 22:27:34 +0000

    styles

---
+++ src/js/component/lights.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Lights = () => {
+    return (
+        <div className="traffic-lights">
+            <div className="rounded-circle lights red">Stop</div>
+            <div className="rounded-circle lights yellow">Slow Down</div>
+            <div className="rounded-circle lights green">GOOOOO!</div>
+        </div>
+    )
+};
+
+export default Lights;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,10 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+import Lights from "./component/lights";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Lights />, document.querySelector("#app"));
